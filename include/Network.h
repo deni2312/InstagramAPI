c2371d695d638b8ab2dad1c501d11f45a81f046e
Denis Beqiraj <denisbeqi2312@gmail.com>
2021-07-04 18:12:01 +0200

    Update Network.h

--- include/Network.h
+++ include/Network.h
@@ -7,7 +7,12 @@ public:
 	Network(const std::string& request);
 	Json::Value sendRequestGet(const std::string& requ,const std::string& body="");
 	Json::Value sendRequestPost(const std::string& requ, const std::string& body = "");
-	struct Error;
+	struct Error : public std::exception{
+		Error(std::string error_code);
+		const char* what() const noexcept override;
+	private:
+		std::string message;
+	};
 private:
 	const std::string request;
 	cpr::Session session;
