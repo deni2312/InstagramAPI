93bb03fa41df173885dda5e2907cddfecae5b27b
Denis Beqiraj <denisbeqi2312@gmail.com>
2021-07-05 00:00:32 +0200

    Delete NetworkInterface.h

--- include/NetworkInterface.h
+++
@@ -1,8 +0,0 @@
-#ifndef NETWORKINTERFACE_H
-#define NETWORKINTERFACE_H
-class NetworkInterface {
-public:
-	virtual Json::Value sendRequestGet(const std::string& requ, const std::string& body = "") = 0;
-	virtual Json::Value sendRequestPost(const std::string& requ, const std::string& body = "") = 0;
-};
-#endif
