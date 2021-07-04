2cb1e22c6efaa5c4995b6e2ff88e2f13069813ab
Denis Beqiraj <denisbeqi2312@gmail.com>
2021-07-04 18:43:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 int main()
 {
 	Instagram instagram{"username", "password"};
+	try{
 	instagram.login();
+	}catch(Network::Error& e){
+		std::cerr<<e.what();
+	}
 	std::string user_id = instagram.search_username("username")["user"]["pk"].asString();
 	instagram.follow(user_id);
 	instagram.media_like("media_id");
