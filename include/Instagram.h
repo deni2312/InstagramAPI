b8a5915aaa5afe1c26c9adbd0f058f9ca4cb9cf9
Denis Beqiraj <denisbeqi2312@gmail.com>
2021-07-04 17:40:55 +0200

    Create Instagram.h

---
+++ include/Instagram.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <exception>
+#include "Network.h"
+#include <memory>
+class Instagram {
+public:
+	Instagram(std::string username,std::string password);
+	void login();
+	Json::Value remove_profile_picture();
+	Json::Value set_private();
+	Json::Value set_public();
+	Json::Value discover();
+	Json::Value follow(const std::string& user_id);
+	Json::Value unfollow(const std::string& user_id);
+	Json::Value block(const std::string& user_id);
+	Json::Value remove_follower(const std::string& user_id);
+	Json::Value unblock(const std::string& user_id);
+	Json::Value show(const std::string& user_id);
+	Json::Value comment_like(const std::string& comment_id);
+	Json::Value comment_unlike(const std::string& comment_id);
+	Json::Value comment_likers(const std::string& comment_id);
+	Json::Value media_info(const std::string& media_id);
+	Json::Value media_like(const std::string& media_id);
+	Json::Value media_unlike(const std::string& media_id);
+	Json::Value media_likers(const std::string& media_id);
+	Json::Value comment(const std::string& media_id,const std::string& comment_text);
+	Json::Value comments(const std::string& media_id);
+	Json::Value get_user_following(const std::string& user_id);
+	Json::Value get_user_followers(const std::string& user_id);
+	Json::Value get_user_feed(const std::string& user_id);
+	Json::Value search_username(const std::string& username);
+	~Instagram();
+private:
+	std::string username;
+	std::string password;
+	std::unique_ptr<Network> network;
+};
